8b86d4bcd2b6a517001c73fc76a2cb2bc7a1dda8
diegocar448 <diegocar448@hotmail.com>
2025-07-20 20:31:31 -0300

    Estrutura inicial do frontend/backend de carrinho/pedidos

--- erpmini.sql
+++ erpmini.sql
@@ -1,18 +1,4 @@
-/*
- Navicat Premium Data Transfer
 
- Source Server         : mysql
- Source Server Type    : MySQL
- Source Server Version : 80042
- Source Host           : localhost:3306
- Source Schema         : laravel
-
- Target Server Type    : MySQL
- Target Server Version : 80042
- File Encoding         : 65001
-
- Date: 20/07/2025 20:06:33
-*/
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
